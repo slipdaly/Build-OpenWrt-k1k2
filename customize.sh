c1670b8f5509eab5f5b35c241c251548cc5aeffa
slipdaly <ghtscgm@aliyun.com>
2026-04-25 21:55:32 +0800

    fix build

--- customize.sh
+++ customize.sh
@@ -21,5 +21,20 @@ git clone --depth 1 "${RTP2HTTPD_REPO_URL}" -b "${RTP2HTTPD_REPO_BRANCH}" "${RTP
 
 cp -a "${RTP2HTTPD_TMP_DIR}/openwrt-support/rtp2httpd-minimal" "${RTP2HTTPD_PKG_DIR}"
 cp -a "${RTP2HTTPD_TMP_DIR}/openwrt-support/luci-app-rtp2httpd-minimal" "${LUCI_RTP2HTTPD_PKG_DIR}"
+rm -rf "${RTP2HTTPD_TMP_DIR}/.git"
 
-rm -rf "${RTP2HTTPD_TMP_DIR}"
+RTP2HTTPD_SOURCE_REF='\$(TOPDIR)/package/_rtp2httpd_src'
+
+if ! grep -q '^RTP2HTTPD_SOURCE_DIR:=' "${RTP2HTTPD_PKG_DIR}/Makefile"; then
+  sed -i "/^PKG_NAME:=rtp2httpd-minimal$/a RTP2HTTPD_SOURCE_DIR:=${RTP2HTTPD_SOURCE_REF}" "${RTP2HTTPD_PKG_DIR}/Makefile"
+else
+  sed -i "s#^RTP2HTTPD_SOURCE_DIR:=.*#RTP2HTTPD_SOURCE_DIR:=${RTP2HTTPD_SOURCE_REF}#" "${RTP2HTTPD_PKG_DIR}/Makefile"
+fi
+sed -i 's#$(CURDIR)/\.\./\.\.#$(RTP2HTTPD_SOURCE_DIR)#g' "${RTP2HTTPD_PKG_DIR}/Makefile"
+
+if ! grep -q '^RTP2HTTPD_SOURCE_DIR:=' "${LUCI_RTP2HTTPD_PKG_DIR}/Makefile"; then
+  sed -i "/^LUCI_NAME:=luci-app-rtp2httpd-minimal$/a RTP2HTTPD_SOURCE_DIR:=${RTP2HTTPD_SOURCE_REF}" "${LUCI_RTP2HTTPD_PKG_DIR}/Makefile"
+else
+  sed -i "s#^RTP2HTTPD_SOURCE_DIR:=.*#RTP2HTTPD_SOURCE_DIR:=${RTP2HTTPD_SOURCE_REF}#" "${LUCI_RTP2HTTPD_PKG_DIR}/Makefile"
+fi
+sed -i 's#$(CURDIR)/\.\./\.\.#$(RTP2HTTPD_SOURCE_DIR)#g' "${LUCI_RTP2HTTPD_PKG_DIR}/Makefile"
